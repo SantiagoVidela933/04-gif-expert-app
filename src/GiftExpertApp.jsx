83feb76b4f3de09daee4c59e56020b90ce7b7766
SantiagoVidela <santi.videla.2@gmail.com>
2023-05-15 10:08:38 -0300

    Se agrega el proyecto

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GiftExpertApp = () => {
+
+
+    const [categories, setCategories] = useState(['Resident Evil', 'Mortal Kombat']);
+
+    const onAddCategory = (newCategory) => {
+
+        if( categories.includes(newCategory) ) return;
+        setCategories([...categories, newCategory]);
+    }
+
+  return (
+    <>
+        <h1>GiftExpertApp</h1>
+    
+        <AddCategory 
+            onNewCategory={(value)=>onAddCategory(value)}
+        />
+
+        {
+            categories.map((category)=>(
+                <GifGrid 
+                    key={category} 
+                    category={category}/>
+            ))
+        }
+  
+
+    </>
+  )
+}
